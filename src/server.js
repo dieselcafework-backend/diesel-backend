2f404f3c5e963307d069bd5eae219b09753c6709
Admin <dieselcafework@gmail.com>
2026-04-21 17:32:22 +0530

    Updated link

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(helmet());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || 'https://dieselcafe.netlify.app',
+    origin: process.env.FRONTEND_URL || 'https://dieselcafee.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
